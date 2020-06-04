73e8b47174f7497e4ad459122b7849a1b855f7fe
Sherub Thakur <sherub.thakur@gmail.com>
2020-06-05 01:06:54 +0530

    First shot at nixy dotfiles

---
+++ home.nix
@@ -0,0 +1,187 @@
+{ config, lib, pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # _1password
+    # NOTE: GUI apps don't show up in spotlight search
+    alacritty
+    bat
+    clojure
+    clojure-lsp
+    docker
+    git
+    fzf
+    # google-chrome
+    jump
+    htop
+    ngrok
+    ripgrep
+    nodejs
+    # postman
+    python3
+    pipenv
+    python3Packages.pip
+    python3Packages.black
+    python3Packages.ipython
+    python3Packages.pynvim
+    python3Packages.isort
+    python3Packages.jedi
+    python3Packages.rope
+    python3Packages.mypy
+    python3Packages.flake8
+    # python3Packages.flake8-annotations
+    # NOTE: Need to add support for mac
+    # spaceship-prompt
+    tmux
+    universal-ctags
+    zsh-syntax-highlighting
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "sherubthakur";
+  home.homeDirectory = "/Users/sherubthakur";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.09";
+
+
+  programs.alacritty = {
+    enable = true;
+    settings = (import ./alacrity-config.nix);
+  };
+
+  programs.bat = {
+    enable = true;
+    config.theme = "OneHalfDark";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Sherub Thakur";
+    userEmail = "sherub.thakur@gmail.com";
+  };
+
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      # Appearance
+      # lightline-vim
+      vim-airline
+      # gruvbox
+      # onehalf
+      vim-one
+      vim-devicons
+
+      # Navigation
+      vim-tmux-navigator
+      vim-sneak
+      fzf-vim
+
+      # Programming
+      vim-elm-syntax
+      # markdown-preview
+      vim-which-key
+      # python-syntax
+      vim-haskellConcealPlus
+      vim-nix
+      coc-nvim
+      # coc-actions
+      coc-eslint
+      # coc-explorer
+      coc-go
+      coc-json
+      # coc-markdownlint
+      coc-pairs
+      coc-prettier
+      coc-python
+      coc-snippets
+      coc-spell-checker
+      coc-tsserver
+      coc-yaml
+
+      # Text objects
+      tcomment_vim
+      vim-surround
+      vim-repeat
+      vim-indent-object
+
+      # vim-floaterm
+      vim-fugitive
+      vim-gitgutter
+    ];
+
+    extraConfig = ''
+      ${builtins.readFile ./nvim/sane_defaults.vim}
+      ${builtins.readFile ./nvim/airline.vim}
+      ${builtins.readFile ./nvim/navigation.vim}
+      ${builtins.readFile ./nvim/theme.vim}
+      ${builtins.readFile ./nvim/coc.vim}
+      ${builtins.readFile ./nvim/which_key.vim}
+    '';
+
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    defaultKeymap = "viins";
+    enableAutosuggestions = true;
+    shellAliases = (import ./zsh_aliases.nix);
+    history.extended = true;
+    plugins = [
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "be3882aeb054d01f6667facc31522e82f00b5e94";
+          sha256 = "0w8x5ilpwx90s2s2y56vbzq92ircmrf0l5x8hz4g1nx3qzawv6af";
+        };
+      }
+    ];
+    oh-my-zsh = {
+    };
+    initExtraBeforeCompInit = ''
+      ${builtins.readFile ./zsh_functions.zsh}
+      ${builtins.readFile ./zsh_secrets.zsh}
+
+      bindkey -M vicmd 'k' history-beginning-search-backward
+      bindkey -M vicmd 'j' history-beginning-search-forward
+
+      eval "$(jump shell)"
+    '';
+    loginExtra = ''
+      ${builtins.readFile ./zsh_prompt.zsh}
+    '';
+    sessionVariables = rec {
+      KEYTIMEOUT=1;
+      DOTFILES="/Users/sherubthakur/.dotfiles";
+
+      PATH=":$HOME/.local/bin:/Users/sherubthakur/.nix-profile/bin:/Users/sherubthakur/Library/Python/3.7/bin:$PATH";
+
+      # Path to your oh-my-zsh installation.;
+      ZSH="/Users/sherubthakur/.oh-my-zsh";
+      NVIM_TUI_ENABLE_TRUE_COLOR=1;
+      EDITOR="nvim";
+      LC_ALL="en_US.UTF-8";
+      LANG="en_US.UTF-8";
+
+      SSL_CERT_FILE=~/.nix-profile/etc/ssl/certs/ca-bundle.crt;
+      NIX_PATH="$HOME/.nix-defexpr/channels:nixpkgs=https://github.com/NixOS/nixpkgs/archive/release-19.03.tar.gz";
+    };
+  };
+
+}
---
+++ zsh_aliases.nix
@@ -0,0 +1,48 @@
+#!/bin/zsh;
+{
+
+  ":q"="exit";
+  vi="nvim";
+  vim="nvim";
+
+  # docker-compose;
+  dc="docker-compose";
+  dcu="docker-compose up --build";
+  dcl="docker-compose logs -f";
+  dcisolated="docker-compose up --build --no-deps consul_common common-redis common_db";
+
+  # Navigation;
+  "..."="cd ../..";
+  "...."="cd ../../..";
+  "....."="cd ../../../..";
+
+  # git;
+  gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+  gc="git commit";
+  gca="git commit --amend";
+  gdc="git diff --cached";
+  gir="git rebase -i";
+  gpr="hub pull-request";
+  gdpr="hub pull-request --draft";
+  gppr="git push origin HEAD && hub pull-request";
+  gpdpr="git push origin HEAD && hub pull-request --draft";
+  gdlocal="git branch --merged | grep -v '\*' | xargs -n 1 git branch -d";
+  gdorigin="git branch -r --merged | grep -v master | sed 's/origin\//:/' | xargs -n 1 -P 5 git push origin";
+  gsur="git submodule update --remote";
+
+  # Grep;
+  grep="grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}";
+
+  # json formatting;
+  json="python -m json.tool";
+
+  # You know (sudoing);
+  # If the last character of the value is a blank, then the next command word following the;
+  # is also checked for expansion.;
+  # So this is just a nice way of making sure your commands are evaluated for aliases before being;
+  # passed over to sudo, which ends up being pretty useful.;
+  sudo="sudo ";
+  fucking="sudo ";
+  holdmybeer="sudo ";
+  servethis="python -c 'import SimpleHTTPServer; SimpleHTTPServer.test()'";
+}
