955e4e8f7b2753f1ee9a499b0942a7bf39a819de
Sherub Thakur <sherub.thakur@gmail.com>
2020-09-04 00:44:54 +0530

    Add shortcut for git checkout

--- zsh/aliases.nix
+++ zsh/aliases.nix
@@ -23,6 +23,7 @@
   # git;
   gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
   gc="git commit";
+  gco="git checkout";
   gs="git status";
   gca="git commit --amend";
   gd="git diff";
